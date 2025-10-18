4143810dafeddf7442ef222ee9dd809fadb07ae7
Dave Paola <dpaola2@gmail.com>
2025-10-18 08:51:23 -0400

    initial iOS app

---
+++ Library/Library/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Library
+//
+//  Created by Dave Paola on 10/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Library/Library/LibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LibraryApp.swift
+//  Library
+//
+//  Created by Dave Paola on 10/18/25.
+//
+
+import SwiftUI
+
+@main
+struct LibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
